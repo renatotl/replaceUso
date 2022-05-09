45d943014e36cef27d4d8d6c11f025e70a254df4
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-05-09 19:44:51 -0300

    comentarios

--- slice.js
+++ slice.js
@@ -1,12 +1,20 @@
 const nome = 'Blue EdTech';
 
+
+
+/* esse primeiro parâmetro pega a primeira letra o "B"  */
 const nomeSlice1 = nome.slice(1);
 console.log(nomeSlice1);
 
+
+/* este SLICE tem 2 parâmetros o primeiro pega o "B" o segundo pega aparti do 3 que é o "e"   */
 const nomeSlice2 = nome.slice(1,3);
 console.log(nomeSlice2);
 
+
+/* este tem 2 parâmetros o 0 não pega nenhum, mas o -3 pega  os três ultimos caracteres */
 const nomeSlice3 = nome.slice(0,-3);
 console.log(nomeSlice3);
 
-//cógido para brincar como funciona o SLICE
+//cógido para brincar como funciona o SLICE.
+
